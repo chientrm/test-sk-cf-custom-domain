146c5640ebc48e986f3e7f5897eef39087cafbfa
chientrm <chientrm@gmail.com>
2022-09-20 04:06:05 +0000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { invalidateAll } from '$app/navigation';
+	import type { PageServerData } from './$types';
+	export let data: PageServerData;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<button on:click={invalidateAll}>Invalidate</button>
+<p>Value: {data.value}</p>
